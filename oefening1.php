b606dc2bc93021dd0cf790911e345103a02f48ed
Aaron de Bruin <122994@student.drenthecollege.nl>
2019-04-01 08:59:33 +0200

    Update oefening1.php

--- oefening1.php
+++ oefening1.php
@@ -1,13 +1,11 @@
 <?php
 
 function maakVierkant() {
-  return $vierkant = "+++++
-                      <br>+++++
-                      <br>+++++
-                      <br>+++++
-                      <br>+++++";
+
+for ($x = 0; $x <= 4; $x++) {
+    echo "+++++ <br>";
+}
 }
 
-  echo maakVierkant();
-
+echo maakVierkant();
 ?>
